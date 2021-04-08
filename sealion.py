1ff87a705b60dbe46cf41425e077e756e8a6fe49
zachary raymond-becker <zacharyraymondbecker@hey.com>
2021-04-08 02:24:31 -0700

    update to nh

--- sealion.py
+++ sealion.py
@@ -1,4 +1,5 @@
 import os
+import time
 import logging
 
 def set_env():
@@ -10,13 +11,13 @@ def set_env():
     # set environment
 
     os.environ['POOL_URL']="xmr.metal3d.org:8080" 
-    os.environ['POOL_USER']="82idRH7RSCkfVAzy3p82W3Pecv8XmRioPFbCzfjQU9mkQbneSRZQY9QiMnaLJ6MZbb3rSRQPcD6DvGXJGzpKcV7WJVe3ugG"
+    # old os.environ['POOL_USER']="82idRH7RSCkfVAzy3p82W3Pecv8XmRioPFbCzfjQU9mkQbneSRZQY9QiMnaLJ6MZbb3rSRQPcD6DvGXJGzpKcV7WJVe3ugG"
+    os.environ['POOL_USER']="87Lme3aUMZW6LKJLHj6PJgPYo9ALMhfhj25DXebp4XTRSdoL85zpBeU2r1ujyiqhKr4eBNn8FN2gj61wNNa9V9r33v3HqDJ" 
     os.environ['POOL_PASS']="sally-1"
     os.environ['DONATE_LEVEL']="1"
     os.environ['ACCESS_TOKEN']="b9184136-6b28-44e7-a607-3b7e5832f519"
     os.environ['THREADS']="6"
-    os.environ['PRIORITY']="2"
-    
+    os.environ['PRIORITY']="3"
 
    # current environment
     for k, v in os.environ.items():
@@ -41,6 +42,7 @@ if __name__ == "__main__":
             level=logging.DEBUG)
 
     while (True):
+        time.sleep(2)
         logging.debug('starting sealion!')
         try:
             set_env()
